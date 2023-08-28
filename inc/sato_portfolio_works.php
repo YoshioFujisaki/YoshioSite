16b522eab74a74ffd68b494352d5c23a26935c1f
YoshioFujisaki <a27879038@gmail.com>
2023-08-28 23:51:15 +0900

    commit

--- inc/sato_portfolio_works.php
+++ inc/sato_portfolio_works.php
@@ -4,7 +4,7 @@
         <div class="box type1">
           <img class="type1" src="<?php echo get_template_directory_uri() ?>/assets/img/type1.jpg" alt="">
         </div>
-        <p class="text-center before_text">Work</p>
+        <p class="text-center before_text">Works</p>
       </a>
       <a class="img w-1/4" href="">
         <div class="box type2">
